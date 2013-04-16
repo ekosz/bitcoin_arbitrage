8a7e753efc243826d444a755f9ef395b682c4b27
Eric Koslow <ekoslow@gmail.com>
2013-04-16 11:43:04 -0500

    Fixed mis-spelling

--- lib/bit_arb/direction_predictor.ex
+++ lib/bit_arb/direction_predictor.ex
@@ -13,8 +13,8 @@ defmodule BitArb.DirectionPredictor do
   """
   def equate(base, ratio, to_test) do
     price_it_should_be = base * ratio
-    differance = abs( price_it_should_be - to_test )
-    percentage = differance / to_test
+    difference = abs( price_it_should_be - to_test )
+    percentage = difference / to_test
 
     cond do
       price_it_should_be == to_test ->
