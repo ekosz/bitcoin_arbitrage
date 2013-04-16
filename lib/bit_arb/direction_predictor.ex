fe0d675a3c6d5342de092d0134e4c172b4d3db2b
Eric Koslow <ekoslow@gmail.com>
2013-04-16 11:42:29 -0500

    Added documentation to direction_predictor.ex

--- lib/bit_arb/direction_predictor.ex
+++ lib/bit_arb/direction_predictor.ex
@@ -1,4 +1,16 @@
 defmodule BitArb.DirectionPredictor do
+  @doc """
+  Gives a prediction that a currency is going to rise or fall
+  depending on a base currency and the normal ratio that the
+  two currencies should have.  If the currency to test is
+  below where it expects it returns that it should rise. If
+  its above, it returns that it should fall.
+
+  ## Example:
+
+      {:fall, amount} = BitArb.DirectionPredictor.equate(100, 0.7, 80)
+
+  """
   def equate(base, ratio, to_test) do
     price_it_should_be = base * ratio
     differance = abs( price_it_should_be - to_test )
